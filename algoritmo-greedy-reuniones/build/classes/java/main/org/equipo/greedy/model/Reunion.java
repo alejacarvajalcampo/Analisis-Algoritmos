75fd5ea18286deefb402e7c8017147b567e35021
Josue Acosta <josueac2001@gmail.com>
2026-08-29 17:42:04 -0500

    feat: add Reunión domain model

---
+++ algoritmo-greedy-reuniones/build/classes/java/main/org/equipo/greedy/model/Reunion.java
@@ -0,0 +1,31 @@
+package com.equipo.greedy.model;
+ 
+public class Reunion {
+ 
+    private String nombre;
+    private int inicio;
+    private int fin;
+ 
+    public Reunion(String nombre, int inicio, int fin) {
+        this.nombre = nombre;
+        this.inicio = inicio;
+        this.fin = fin;
+    }
+ 
+    public String getNombre() {
+        return nombre;
+    }
+ 
+    public int getInicio() {
+        return inicio;
+    }
+ 
+    public int getFin() {
+        return fin;
+    }
+ 
+    @Override
+    public String toString() {
+        return nombre + " (" + inicio + ":00 - " + fin + ":00)";
+    }
+}
